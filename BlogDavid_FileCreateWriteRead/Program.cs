de88bcc14a1748f782af04635459930d993f32dc
DAVID-PC\david <david.bollobas@gmail.com>
2022-09-16 13:08:17 +0200

    init

---
+++ BlogDavid_FileCreateWriteRead/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace BlogDavid_FileCreateWriteRead
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Keep info of file in memory
+            var fileInfo = new FileInfo("./a.txt");
+            // Create the file, then create a stream writer, which is easily used to add text
+            using (var fileStream = fileInfo.Create())
+            using (var streamWriter = new StreamWriter(fileStream))
+            {
+                streamWriter.WriteLine("Test data!");
+            }
+            // StreamReader is a great tool to read text from file
+            using (var streamReader = new StreamReader(fileInfo.FullName))
+            {
+                Console.WriteLine(streamReader.ReadToEnd());
+            }
+
+            // Don't let console to close, wait for one char
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BlogDavid_FileCreateWriteRead/README.md
@@ -0,0 +1,1 @@
+﻿This repo is created for this blog post: https://blog.davidbollobas.hu/2022/10/04/create-write-read-text-file-using-c/
